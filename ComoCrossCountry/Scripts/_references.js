617774bb518298baa4b983eba37f3cf4bea65743
steve <smann004@yahoo.com>
2022-07-03 22:24:17 -0500

    updating packages and site

--- ComoCrossCountry/Scripts/_references.js
+++ ComoCrossCountry/Scripts/_references.js
@@ -1,4 +1,5 @@
-﻿/// <reference path="jquery-1.6.2.js" />
+﻿/// <reference path="modernizr-2.8.3.js" />
+/// <reference path="jquery-3.6.0.js" />
 /// <reference path="jquery-ui-1.8.11.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="knockout-2.0.0.debug.js" />
